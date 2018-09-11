7bd7cdd6198324fc3b9c103c80943decc7ebb016
Casper Andersen <casp7654@zbc.dk>
2018-09-11 16:41:40 +0200

    Item added

--- FileExplorer/FileExplorer/Logic/Item/Item.cs
+++ FileExplorer/FileExplorer/Logic/Item/Item.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileExplorer.Logic.Model
+namespace FileExplorer.Logic.Item
 {
-    class Item
+    abstract class Item
     {
+        private string path;
+        private string name;
+        private string size;
+        private string owner;
+        private FileType type;
+        private DateTime creationDate;
+        private bool isWriteProtected;
+
+        internal string Path { get; set; }
+        internal string Name { get; set; }
+        internal string Size { get; set; }
+        internal string Owner { get; set; }
+        internal FileType Type { get; set; }
+        internal DateTime CreationDate { get; set; }
+        internal bool IsWriteProtected { get; set; }
+
+        public abstract void Open();
     }
 }
