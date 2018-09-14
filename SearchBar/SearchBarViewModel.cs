49341c1c3670f4fc6f39445c1f70e05e374ba84d
Casper Andersen <casp7654@zbc.dk>
2018-09-14 10:10:21 +0200

    SearchBar

---
+++ SearchBar/SearchBarViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTestApp.Import;
+
+namespace WpfTestApp.ViewModel
+{
+    class SearchBarViewModel
+    {
+        private string _searchParam;
+
+        public string SearchParam { get { return this._searchParam; } set { this._searchParam = value; /*OnPropertyChanged("SearchParam");*/ } }
+
+        public DelegateCommand SearchCommand { get; set; }
+
+        public SearchBarViewModel()
+        {
+            SearchCommand = new DelegateCommand(SearchFor);
+        }
+
+        private void SearchFor(object obj)
+        {
+            this._searchParam = obj.ToString(); // MAGIC HAPPENS HERE
+        }
+    }
+}
